f0c6d30477111f1e2f27ea83f14ae62bf3023935
Severin Langberg <langberg91@gmail.com>
2020-03-06 09:09:53 +0100

    BM: Init draft for HMM synth data gen.

--- generate.py
+++ generate.py
@@ -1,176 +1,222 @@
-import numpy as np 
+from typing import List
 
-from transition import set_init_state, sample_scores
+import numpy as np
 
-
-def data_generator(num_samples, num_tpoints, missing=0):
-	
-    X = np.ones((num_samples, num_tpoints), dtype=np.int32) * missing
-
-    # Initial female condition.
-    X, idx = set_init_state(X, return_idx=True)
-
-    for num in range(num_samples):
-
-        # Next time points.
-        next_screen = idx + num + 1
-
-        # Valid time points < num_tpoints.
-        idx = next_screen[next_screen < num_tpoints + 1]
-
-        X = sample_scores(X, idx)
-
-    return X
+from probas import lambda_sr, p_init_state
 
 
-def set_init_state(X, seed=None, return_idx=False):
+age_partitions = np.array([
+    (16, 19),  
+    (20, 24),
+    (25, 29),
+    (30, 34), 
+    (35, 39), 
+    (40, 49), 
+    (50, 59), 
+    (60, 96)
+])
 
-    # Time of first screening.
-    init_idx = init_idx(num, seed=seed)
+
+def inital_state(init_age: int, seed: int = 0):
+    """Sample state at first screening.
     
-    # State at initial screening.
-    X[init_idx] = init_state(num, seed=seed)
-
-    if return_idx:
-        return X, init_idx
-
-    return X
-
-
-def sample_scores(X, idx):
-
-    prev_states = X[idx]
-
-        
-
-
-
-
-def init_state(x, time_index, df_init):
-    """Assings intial state according to empirical probabilities of being in a
-    particular state at the time of the first screening."""
-
-    if seed is None:
-        np.random.seed(SEED)
-    else:
-        np.random.seed(seed)
-    
-    new_state = np.random.choice([1, 2, 3, 4], p=df_init.iloc[:, time_index])
-    
-    x[time_index] = int(new_state)
-
-
-
-def sample_scores(X, num_tpoints):
-
-    pass
- 
-
-def _impute_trajectory(x, otrain, Ptransition, force_init=False, break_at_dropout=True):
-    """
     Args:
-        x: A row in X.
-        otrain: A row in Otrain.
+        x:
+        
+    Returns:
+        
+    """
+    
+    np.random.seed(seed)
+    
+    age_grp = age_group_idx(init_age)
+                  
+    return np.random.choice([1, 2, 3, 4], p=p_init_state[age_grp])       
+
+
+# TODO: Handle out of bounds age grouping.
+def age_group_idx(age: int):
+    """Returns index for the age group."""
+    
+    for num, (tau_p, tau_pp) in enumerate(age_partitions):
+        
+        if age in range(tau_p, tau_pp + 1):
+            return num
+        
+    #raise ValueError(f'Could not find any group for age {age}')        
+    return num
+
+
+# ERROR: Potentially something wrong with def of transit probas.
+def legal_transitions(current_state: int, lambdas: List) -> List:
+    
+    # TEMP:
+    return lambdas
+    
+    # s1 => s2
+    if current_state == 1:
+        return [lambdas[0]]
+    
+    # s2 => s3 or s2 => s1
+    if current_state == 2:
+        return [lambdas[1], lambdas[3]]
+    
+    # s3 => s4 or s3 => s2
+    if current_state == 3:
+        return [lambdas[2], lambdas[4]]
+
+
+def kappa_0(init_age, current_state, t):
+
+    l = age_group_idx(init_age + t)
+    
+    s = sum(legal_transitions(current_state, lambda_sr[l]))
+
+    return -1.0 * t * s
+
+
+def kappa_1(age, current_state, t):
+    
+    k = age_group_idx(age)
+    tau_kp = age_partitions[k + 1][0]
+
+    l = age_group_idx(age + t)
+    tau_l = age_partitions[l][0]
+    
+    s_k = (age - tau_kp) * sum(legal_transitions(current_state, lambda_sr[k]))
+    s_l = (tau_l - age) * sum(legal_transitions(current_state, lambda_sr[l]))
+    
+    return s_k + s_l
+    
+
+def kappa_m(age, current_state, m):
+    
+    k = age_group_idx(age)
+    km = k + m
+    kmm = km - 1
+    
+    tau_km = age_partitions[km][1]
+    tau_kmm = age_partitions[kmm][0]
+
+    s_kmm = sum(legal_transitions(current_state, lambda_sr[kmm, :]))
+    
+    return (tau_kmm - tau_km) * s_kmm
+
+
+def kappa(init_age, current_state, t, m):
+    
+    if m == 0:
+        return kappa_0(init_age, current_state, t)
+    
+    if m == 1:
+        return kappa_1(init_age, current_state, t)
+    
+    return kappa_m(init_age, current_state, m)
+
+
+def cumul_sojourn_time(n: int, current_age: int, current_state: int, t: int) -> float:
+    """Cumulative distribution for the sojourn time.
+    
+    Args:
+        n: Number of additional age partitinos covered by the time lapse `t`
+            when starting at age `current_age`.
+        current_age:
+        current_state:
+        t: Time passed since `current_age`.
+    
+    Returns:
+        The cumulative distribution for the sojourn time evaluated at time t.
     """
 
-    # Impute x[0] with a randomly selected value.
-    if force_init:
-        set_init_state(x, time_index=0)
+    # NB: Range over n + 1 for additional age paritions <= 7 (as expected).
+    kappas = [kappa(current_age, current_state, t, i) for i in range(n + 1)]
+    
+    return 1.0 - np.exp(sum(kappas))
+    
 
-    if break_at_dropout:
-        nz = x.nonzero()[0]
-    else:
-        nz = np.append(x.nonzero()[0], len(x))
+def sojourn_time_cdf(start_age, stop_age, current_state):
+    
+    time_lapse = int(stop_age - start_age)
 
-    for num, nz_idx in enumerate(nz[:-1]):
+    cdf = np.zeros(time_lapse, dtype=np.float32)
+    
+    k = age_group_idx(start_age)
+    for t in range(1, time_lapse):
         
-        next_nz_idx = int(nz[num + 1])
+        l = age_group_idx(start_age + t)
         
-        # No need to impute.
-        if next_nz_idx - nz_idx < 2:
-            continue
+        cdf[t] = cumul_sojourn_time(l - k, start_age, current_state, t)
 
-        prev_state = int(x[nz_idx])
-        for time_index in range(nz_idx + 1, next_nz_idx):
-            
-            if prev_state == 1:
-                state = shift_from_s1(time_index, Ptransition)
-            
-            elif prev_state == 2:
-                state = shift_from_s2(time_index, Ptransition)
-            
-            elif prev_state == 3:
-                state = shift_from_s3(time_index, Ptransition)
-
-            else:
-                continue
-
-            x[time_index] = state
-            otrain[time_index] = 1
-
-            prev_state = state
+    return np.array(cdf)
 
 
-def impute(X, O_train, O_test):
+# QUESTION: n = l - k + 1 or n = l - k?
+def sojourn_time(sojourn_cdf: np.ndarray, start_age: int,
+                 current_state: int, seed: int = 0) -> float:
+    """Estimate the time spent in a given state.
 
-    p_transit = get_transition_probas()
+    Args:
+        sojourn_cdf: Cumulative 
+        
+    Returns:
+        The amount of time a female spends in the current state.
+    """
 
-    X_imp = X.copy()
-    O_imp = O_train.copy()
+    np.random.seed(seed)
+    u = np.random.uniform(low=0.0, high=1.0)
+    
+    t_lower = np.squeeze(np.where(u > sojourn_cdf))[-1]
 
-    start_idx = np.argmax(O_train, axis=1)
-    end_idx = np.argmax(np.cumsum(O_train, axis=1), axis=1)
+    l = age_group_idx(start_age + t_lower)
+    k = age_group_idx(start_age)
+    print(l, k)
+    # QUESTION: n = l - k + 1 or n = l - k? Using n = l - k + 1 gives less variance.
+    s = sum([kappa(start_age, current_state, start_age + t_lower, i) for i in range(1, l - k + 1)])
+    scale = sum(legal_transitions(current_state, lambda_sr[l, :]))
 
-    for num, (x, o) in enumerate(zip(X_imp, O_imp)):
-        impute_trajectory(x, o, start_idx[num], end_idx[num], p_transit)
-
-    X_imp[O_train.nonzero()] = X[O_train.nonzero()]
-    X_imp[O_test.nonzero()] = X[O_test.nonzero()]
-
-    return X_imp, O_imp
-
-
-def impute_trajectory(x, o, start_idx, end_idx, p_transit, missing=0):
-
-    idx = start_idx
-    while idx < end_idx:
-
-        # Propagate zeros after cancer cases.
-        current_score = x[idx]
-        if current_score == 0:
-            idx = idx + 1
-
-            continue
-
-        next_score = x[idx + 1]
-        if next_score == missing:
-            x[idx + 1] = sample_next_score(current_score, idx + 1, p_transit=p_transit)
-
-        idx = idx + 1
-
-    o[start_idx:end_idx] = 1
+    return (s - np.log(1 - u)) / scale
 
 
-def sample_next_score(current_score, next_idx, p_transit, num_iter=10):
+def next_state(age: int, current_state: int, seed: int = 0) -> int:
+    """Simulate the next state from sojourn time conditions.
 
-    np.random.seed(0)
+    Args:
+        age:
+        current_state: 
+        seed: Reproduce the pseudo-random number generator.
 
-    scores = []
-    for _ in range(num_iter):
+    Returns:
+        The next state.
 
-        if current_score == 1:
-            next_state = shift_from_s1(next_idx, p_transit)
+    Note:
+        * Consider only valid transitions.
+        * Assume successful treatment if cancer (s4) and transits to normal state.
+    """
 
-        elif current_score == 2:
-            next_state = shift_from_s2(next_idx, p_transit)
+    # s1 -> s2
+    if current_state == 1:
+        return 2
 
-        elif current_score == 3:
-            next_state = shift_from_s3(next_idx, p_transit)
+    # NB: Assume successful treatment and transits to normal state.
+    if current_state == 4:
+        return 1
 
-        else:
-            # Propagate zeros after cancer cases.
-            next_state = 0
+    np.random.seed(seed)
 
-        scores.append(next_state)
+    age_group = age_group_idx(age)
 
-    return np.random.choice(scores)
+    # s2 -> s3 or s2 -> s1
+    if current_state == 2:
+        lambdas = [lambda_sr[age_group, 1], lambda_sr[age_group, 3]]
+    
+    # s3 -> s4 or s3 -> s2
+    if current_state == 3:
+        lambdas = [lambda_sr[age_group, 2], lambda_sr[age_group, 4]]
+    
+    # s2 -> s3 or s2 -> s1
+    if current_state == 2:
+        return np.random.choice((3, 1), p=lambdas / sum(lambdas))
+    
+    # s3 -> s4 or s3 -> s2
+    if current_state == 3:
+        return np.random.choice((4, 2), p=lambdas / sum(lambdas))
--- main.py
+++ main.py
@@ -1,11 +1,54 @@
-from generate import data_generator
+from typing import Union, List
+
+import numpy as np
+
+from generate import (
+	sojourn_time_cdf, sojourn_time, 
+	next_state, inital_state
+)
 
 
+# TEMP: Current start_period and end_period assumes x has 80 entries but can be scaled
+# to increase resolution.
+def simulate_profile(x: Union[List, np.ndarray], current_age=16, age_max=96):
+    """Update the profile vector of a single female. 
 
-def make_data():
-	
-	data_generator()
+    Args:
+    	x: Female profile vector.
+    	current_age:
+		age_max:
+    """
+
+    # Set state at first screening.
+    current_state = inital_state(init_age=current_age)
+    
+    start_period = 0
+    end_period = 1
+    
+    while x[-1] == 0:
+        
+        # Time spent in current state.
+        cdf = sojourn_time_cdf(current_age, age_max, current_state)
+        
+        dt = sojourn_time(cdf, current_age, current_state)
+        
+        end_period = end_period + int(dt)
+        
+        # Update profile values with current state.
+        x[start_period:end_period] = current_state
+
+        current_age = current_age + int(dt)
+        current_state = next_state(current_age, current_state=current_state)
+        
+        start_period = end_period
 
 
 if __name__ == '__main__':
-	make_data()
+
+	# Simulate five females over 80 years (16-96 yo)
+	X = np.zeros((5, 80), dtype=np.int32)
+
+	for x in X:
+	    simulate_profile(x, current_age=16, age_max=96)
+
+	print(X)
