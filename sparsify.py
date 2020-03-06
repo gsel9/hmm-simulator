e63d629841fea6d6afc427d1e09421fef1fc9e32
Severin Langberg <langberg91@gmail.com>
2020-03-06 09:48:49 +0100

    TODO: Sparsen pofiles

---
+++ sparsify.py
@@ -0,0 +1,8 @@
+# TODO: Simulate dropout time with data from empirical dropout dist.
+def dropout():
+	pass
+
+
+# TODO: Randomly remove screenings according to some criteria.
+def irregular_screenings():
+	pass
